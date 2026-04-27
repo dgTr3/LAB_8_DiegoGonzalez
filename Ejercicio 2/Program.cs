fa19c55fc1228e37535c47a2a1657819ccdbcbad
dgTr3 <Edugta31@gmail.com>
2026-04-27 11:14:56 -0600

    Ejercicio 2 completo

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Ventas de productos por día matrices");
+Console.WriteLine();
+Console.WriteLine("Ingrese el número de productos: ");
+int productos=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el número de días: ");
+int dias=int.Parse(Console.ReadLine());
+double[,] ventas=new double[productos,dias];
+for (int i=0; i<productos; i++)
+{
+    for (int j=0; j<dias; j++)
+    {
+        Console.WriteLine($"Ingrese las ventas del producto {i+1} del día {j+1}");
+        ventas[i,j]=double.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("Indique que producto quiere ver: ");
+int selecc=int.Parse(Console.ReadLine());
+Console.WriteLine($"Muestra del día seleccionado: ");
+for (int c=0; c<dias; c++)
+{
+    Console.Write(ventas[selecc-1,c]+" ");
+}
